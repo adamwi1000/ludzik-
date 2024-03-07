35c6f24fc78e8ae923f82cd58e0ab15ca17f6ba8
adamwi1000 <adamwi1000@gmai.com>
2024-03-08 00:18:42 +0100

    kod

---
+++ Assets/codes/game_over.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game_over : MonoBehaviour
+{
+    [SerializeField] move_frog frog;
+    public GameObject gameoverS;
+    
+
+    public void gameover()
+    {
+        
+
+
+        
+        
+            gameoverS.SetActive(true);
+        
+        
+    }
+    
+
+
+}
